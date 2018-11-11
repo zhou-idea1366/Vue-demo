6b150884a934a633d08bcc5540b433b736b989ef
Mai <1670470524@qq.com>
2018-11-11 16:45:18 +0800

    todos静态资源完成

---
+++ VueDemo/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="todo-container">
+      <div class="todo-wrap">
+        <TodoHeader/>
+        <TodoMain/>
+        <TodoFooter/>
+
+      </div>
+    </div>
+</template>
+
+<script>
+  import Header from './components/Header.vue'
+  import Footer from './components/Footer.vue'
+  import Main from './components/Main.vue'
+
+
+
+  export default {
+    components:{
+      TodoHeader:Header,
+      TodoFooter:Footer,
+      TodoMain:Main
+    }
+  }
+
+</script>
+
+<style scoped>
+  .todo-container {
+    width: 600px;
+    margin: 0 auto;
+  }
+  .todo-container .todo-wrap {
+    padding: 10px;
+    border: 1px solid #ddd;
+    border-radius: 5px;
+  }
+</style>
